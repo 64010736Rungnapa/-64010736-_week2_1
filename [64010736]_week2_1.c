c196201f7d66deb9c03ada7c9f85d50cf4515a92
64010736Rungnapa <88958166+64010736Rungnapa@users.noreply.github.com>
2021-08-15 12:11:12 +0700

    Add files via upload

---
+++ [64010736]_week2_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main(){
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+	printf("\n Hello world");
+}
